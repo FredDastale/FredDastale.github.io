da510af7aeb880b7be7e9b4a38580d1812427ada
FredDastale <148912845+FredDastale@users.noreply.github.com>
2023-10-25 00:08:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,15 @@
 *This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 License
 */
 
-$(function(){
-    $("#console").on("input", function(event) {
-        Typer.addText(event.originalEvent.data); // Cattura l'input e chiama la funzione addText
-    });
-});
+$(
+	function(){
+		$( document ).keydown(
+			function ( event ) {
+				Typer.addText( event ); //Capture the keydown event and call the addText, this is executed on page load
+			}
+		);
+	}
+);
 
 var Typer={
 	text: null,
@@ -59,25 +63,25 @@ var Typer={
         Typer.deniedCount=0; //reset access denied count
 	},
 
-	addText:function(input){//Main function to add the code
+	addText:function(key){//Main function to add the code
 	  var console=$("#console")
-		if (input === 'Alt'){// key 18 = alt key
+		if(key.key==='Alt'){// key 18 = alt key
 			Typer.accessCount++; //increase counter
 			if(Typer.accessCount>=3){// if it's pressed 3 times
 				Typer.makeAccess(); // make access popup
 			}
-		}else if (input === 'CapsLock')){// key 20 = caps lock
+		}else if(key.key==='CapsLock'){// key 20 = caps lock
 			Typer.deniedCount++; // increase counter
 			if(Typer.deniedCount>=3){ // if it's pressed 3 times
 				Typer.makeDenied(); // make denied popup
 			}
-		}else if (input === 'Esc'){ // key 27 = esc key
+		}else if(key.key==='Esc' || key.key==='Escape'){ // key 27 = esc key
 			Typer.hidepop(); // hide all popups
 		}else if(Typer.text){ // otherwise if text is loaded
 			var cont=Typer.content(); // get the console content
 			if(cont.substring(cont.length-1,cont.length)==="|") // if the last char is the blinking cursor
 				console.html(console.html().substring(0,cont.length-1)); // remove it before adding the text
-			if(input !== 'Backspace'){ // if key is not backspace
+			if(key.key!=='Backspace'){ // if key is not backspace
 				Typer.index+=Typer.speed;	// add to the index the speed
 			}else{
 				if(Typer.index>0) // else if index is not less than 0
@@ -90,6 +94,12 @@ var Typer={
       console.html(text.replace(rtn,"<br/>").replace(rtt,"&nbsp;&nbsp;&nbsp;&nbsp;").replace(rts,"&nbsp;"));// replace newline chars with br, tabs with 4 space and blanks with an html blank
 			window.scrollBy(0,50); // scroll to make sure bottom is always visible
 		}
+		if ( key.preventDefault && key.key !== 'F11' ) { // prevent F11(fullscreen) from being blocked
+			key.preventDefault()
+		}
+		if(key.key !== 'F11'){ // otherwise prevent keys default behavior
+			key.returnValue = false;
+		}
 	},
 
 	updLstChr:function(){ // blinking cursor
@@ -100,8 +110,4 @@ var Typer={
 		else
 			this.write("|"); // else write it
 	}
-};
-	document.addEventListener('touchstart', ( event ) => {
-  document.querySelector('.style').style.display = 'none';
-  Typer.addText( event ); //Capture the tap event for mobileand call the addText, this is executed on page load
-});
+}
